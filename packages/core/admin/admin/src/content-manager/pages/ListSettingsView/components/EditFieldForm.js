3b9ba5745747475679f99c437250ce787e25dadf
Jamie Howard <jamie.howard@strapi.io>
2022-12-16 11:12:55 +0000

    fix(content-manager): close ListSettingsView/EditFieldForm with escape key

--- packages/core/admin/admin/src/content-manager/pages/ListSettingsView/components/EditFieldForm.js
+++ packages/core/admin/admin/src/content-manager/pages/ListSettingsView/components/EditFieldForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 import upperFirst from 'lodash/upperFirst';
+import isEmpty from 'lodash/isEmpty';
 import styled from 'styled-components';
 import {
   ModalLayout,
@@ -36,6 +37,10 @@ const EditFieldForm = ({
   type,
 }) => {
   const { formatMessage } = useIntl();
+
+  if (isEmpty(fieldToEdit)) {
+    return null;
+  }
 
   const relationType = attributes[fieldToEdit].relationType;
 
